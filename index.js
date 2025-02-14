b14e3834c08572d8e854f2aa7dc63990caaa3387
Zülal Özçelik <159713348+zulalozcelik@users.noreply.github.com>
2025-02-14 14:57:44 +0300

    Digital Clock

--- index.js
+++ index.js
@@ -56,7 +56,7 @@
 //let randomNum;
 
 //myButton.onclick= function(){
-    //randomNum= Math.floor(Math.random()* max) + min; 
+    //randomNum= Math.floor(Math.random()* max) + min;
     //myLabel.textContent=randomNum;
     //
     //
@@ -73,10 +73,10 @@
 //let running = true;
 
 //while(running){
-//    
+//
 //    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
 //    guess = Number(guess);
-//    
+//
 //    if(isNaN(guess)){
 ////window.alert("Please enter a valid number");
 //    }
@@ -120,7 +120,7 @@
 //////result.textContent = temp.toFixed(1) + "°C";
 ////}
 ////else{
-//////result.textContent = "Select a unit"; 
+//////result.textContent = "Select a unit";
 ////}
 //}
 
@@ -198,35 +198,52 @@
 
 //console.log(`Current count: ${counter.getCount()}`);
 
-function calculate(){
-    const totalAmount = document.getElementById("total-amount");
-    const principalInput = document.getElementById("principal");
-    const rateInput = document.getElementById("rate");
-    const yearsInput = document.getElementById("years");
+//function calculate(){
+//    const totalAmount = document.getElementById("total-amount");
+//    const principalInput = document.getElementById("principal");
+//    const rateInput = document.getElementById("rate");
+//    const yearsInput = document.getElementById("years");
 
-    let principal= Number(principalInput.value);
-    let rate = Number(rateInput.value /100) ;
-    let years= Number(yearsInput.value);
+//    let principal= Number(principalInput.value);
+//    let rate = Number(rateInput.value /100) ;
+//    let years= Number(yearsInput.value);
 
-    if(principal < 0 || isNaN(principal)){
-        principal = 0;
-        principalInput.value=0;
-    }
+//    if(principal < 0 || isNaN(principal)){
+//        principal = 0;
+//        principalInput.value=0;
+//    }
 
-    if(rate < 0 || isNaN(rate)){
-        rate = 0;
-        rateInput.value = 0;
-    }
+//    if(rate < 0 || isNaN(rate)){
+//        rate = 0;
+//        rateInput.value = 0;
+//    }
 
-    if(years < 0 || isNaN(years)){
-        years = 0;
-        yearsInput.value = 0;
-    }
+//    if(years < 0 || isNaN(years)){
+//        years = 0;
+//        yearsInput.value = 0;
+//    }
 
 
-    const result = principal * Math.pow((1 + rate / 1), 1 * years)
+//    const result = principal * Math.pow((1 + rate / 1), 1 * years)
 
-    totalAmount.textContent= result.toLocaleString(undefined,
-                                                    {style:"currency",
-                                                    currency: "USD"}); 
+//    totalAmount.textContent= result.toLocaleString(undefined,
+//                                                    {style:"currency",
+//                                                    currency: "USD"});
+//}
+
+//*******************CLOCK********************
+function updateClock() {
+
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById("clock").textContent = timeString;
 }
+
+updateClock();
+setInterval(updateClock, 1000);
